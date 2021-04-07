25858d810f6a47570ce07b162f3f7f423353167d
Elena-GHub <60514098+Elena-GHub@users.noreply.github.com>
2021-04-07 20:06:10 +0200

    Added test file for DinamicLoading

---
+++ java/Mod1/1.06/src/test/java/tests/TestDynamicLoading.java
@@ -0,0 +1,38 @@
+package tests;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import pageobjects.DynamicLoading;
+
+import static org.junit.Assert.assertTrue;
+
+public class TestDynamicLoading {
+
+    private WebDriver driver;
+    private DynamicLoading dynamicLoading;
+
+    @Before
+    public void setUp() {
+        System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
+        ChromeOptions browserOptions = new ChromeOptions();
+        browserOptions.setCapability("browserVersion", "86.0");
+        driver = new ChromeDriver(browserOptions);
+        dynamicLoading = new DynamicLoading(driver);
+    }
+
+    @Test
+    public void hiddenElementLoads() {
+        dynamicLoading.loadExample("1");
+        assertTrue("finish text didn't display after loading",
+                dynamicLoading.finishTextPresent());
+    }
+
+    @After
+    public void tearDown() {
+        driver.quit();
+    }
+}
